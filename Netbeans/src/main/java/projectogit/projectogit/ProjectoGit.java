1fba8bd2c265d5702a70cf05f66a6fd284862108
salvador <salvazcon@gmail.com>
2023-12-07 17:58:10 +0100

    Proyecto Completo

---
+++ Netbeans/src/main/java/projectogit/projectogit/ProjectoGit.java
@@ -0,0 +1,13 @@
+package projectogit.projectogit;
+
+/**
+ * @author salvador azcon
+ */
+public class ProjectoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+	System.out.println("Cambios");
+	System.out.println("Cambios desde github");
+    }
+}
---
+++ VScode/projectoGit_VScode.c
@@ -0,0 +1,10 @@
+#include "unistd.h"
+#include "stdio.h"
+
+int main ()
+{
+    printf("Hola mundo !\n");
+    printf("Cambios !\n");
+    printf("Cambios en github!\n");
+    return(0);
+}
