c02e1a6109ca7788ab2fc315183a75369f2be609
salvador <salvazcon@gmail.com>
2023-12-07 16:41:28 +0100

    Projecto Netbeans

--- src/main/java/projectogit/projectogit/ProjectoGit.java
+++ src/main/java/projectogit/projectogit/ProjectoGit.java
@@ -7,6 +7,6 @@ public class ProjectoGit {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	System.out.println("Cambio realizado.");
+	System.out.println("Cambio realizados");
     }
 }
