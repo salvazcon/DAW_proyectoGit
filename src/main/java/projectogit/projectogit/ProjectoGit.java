a230c4bedfada83f1348b29c564cdce088599515
salvador <salvazcon@gmail.com>
2023-12-07 16:21:34 +0100

    Projecto Netbeans

---
+++ src/main/java/projectogit/projectogit/ProjectoGit.java
@@ -0,0 +1,12 @@
+package projectogit.projectogit;
+
+/**
+ * @author salvador azcon
+ */
+public class ProjectoGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+	System.out.println("Cambio realizado");
+    }
+}
